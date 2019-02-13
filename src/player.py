4069b7c572d587077170fe7413dc2e3324e62321
Chris Cedeno <chrisrey95@gmail.com>
2019-02-13 16:10:01 -0500

    Tasks scaffolding for player and room classes

--- src/player.py
+++ src/player.py
@@ -1,2 +1,11 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+class Player:
+    def __init__(self, room):
+        self.room = room
+        # TODO player can carry items
+    
+    #TODO player can pickup items
+
+    #TODO player can frop items
--- src/room.py
+++ src/room.py
@@ -1,2 +1,13 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+class Room:
+    def __init__(self, name, description):
+        self.name = name
+        self.description: description
+        # TODO have room hold items
+    
+    # TODO room can drop items
+
+    # TODO room can get items
+    
